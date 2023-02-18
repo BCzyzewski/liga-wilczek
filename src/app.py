205ca42e0a4b00588313d36b2d6c408cb9f76bfa
Bartek C <bartlomiejczyzewski@outlook.com>
2023-02-18 20:40:40 +0100

    Added_Password_Protect

--- src/app.py
+++ src/app.py
@@ -10,6 +10,34 @@ from datetime import datetime
 import pytz
 
 
+def check_password():
+    """Returns `True` if the user had the correct password."""
+
+    def password_entered():
+        """Checks whether a password entered by the user is correct."""
+        if st.session_state["password"] == st.secrets["password"]:
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]  # don't store password
+        else:
+            st.session_state["password_correct"] = False
+
+    if "password_correct" not in st.session_state:
+        # First run, show input for password.
+        st.text_input(
+            "Password", type="password", on_change=password_entered, key="password"
+        )
+        return False
+    elif not st.session_state["password_correct"]:
+        # Password not correct, show input + error.
+        st.text_input(
+            "Password", type="password", on_change=password_entered, key="password"
+        )
+        st.error("😕 Password incorrect")
+        return False
+    else:
+        # Password correct.
+        return True
+
 
 def write_color(color_name: str, text: str):
 
@@ -89,11 +117,13 @@ def second_tab(tab: st.tabs):
 
 def create_dashboard():
 
-    tab1, tab2 = st.tabs(["Welcome", "Results"])
+    if check_password():
 
-    first_tab(tab1)
+        tab1, tab2 = st.tabs(["Welcome", "Results"])
 
-    second_tab(tab2)
+        first_tab(tab1)
+
+        second_tab(tab2)
 
 
 create_dashboard()    
