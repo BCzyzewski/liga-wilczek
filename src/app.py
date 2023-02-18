c3ed9d56ef965cf2525779685e48a1dc9fdda1c0
Bartek C <bartlomiejczyzewski@outlook.com>
2023-02-18 11:58:35 +0100

    Added_result_table

--- src/app.py
+++ src/app.py
@@ -2,8 +2,11 @@ from csv import writer
 import streamlit as st
 from streamlit_extras.colored_header import colored_header
 from streamlit_extras.no_default_selectbox import selectbox
+from streamlit_extras.add_vertical_space import add_vertical_space
+
 import pandas as pd
 
+from datetime import datetime
 
 
 
@@ -12,12 +15,12 @@ def write_color(color_name: str, text: str):
     return st.markdown(f'<h1 style="color:{color_name};font-size:15px;">{text}</h1>', unsafe_allow_html=True)
 
 
-def save_result_to_db(player_1: str, result_1: int, result_2: int, player_2: str):
+def save_result_to_db(player_1: str, result_1: int, result_2: int, player_2: str, datetime: str):
     with open('results.csv', 'a', newline='\n') as f_object:
 
         writer_object = writer(f_object)
     
-        writer_object.writerow([player_1, result_1, result_2, player_2])
+        writer_object.writerow([player_1, result_1, result_2, player_2, datetime])
     
         f_object.close()
 
@@ -41,42 +44,46 @@ def second_tab(tab: st.tabs):
         description="Tutaj zapiszesz oraz zobaczysz wyniki meczów",
         color_name="violet-70")
 
-        with st.form("submit_match", clear_on_submit=True):
+        with st.container():
 
-            col1, col2, col3, col4 = st.columns(4)
+            with st.form("submit_match", clear_on_submit=True):
 
-            with col1:
-                player_1 = selectbox("Gracz 1", ["A", "B", "C"])
+                col1, col2, col3, col4 = st.columns(4)
 
-            with col2:
-                result_1 = st.number_input('Wygrane: G1', step = 1, min_value = 0, max_value = 5)
+                with col1:
+                    player_1 = selectbox("Gracz 1", ["A", "B", "C"])
 
-            with col3:
-                result_2 = st.number_input('Wygrane: G2', step = 1, min_value = 0, max_value = 5)
+                with col2:
+                    result_1 = st.number_input('Wygrane: G1', step = 1, min_value = 0, max_value = 5)
 
-            with col4:        
-                player_2 = selectbox("Gracz 2", ["A", "B", "C"])
+                with col3:
+                    result_2 = st.number_input('Wygrane: G2', step = 1, min_value = 0, max_value = 5)
 
-
-            submitted = st.form_submit_button("Submit")
-            if submitted:
-                if result_1 + result_2 > 5:
-                    st.write(f'Niepoprawny wynik! Zbyt duża ilość gier. Spróbuj ponownie.')
-                    write_color("red", f'Niepoprawny wynik! Zbyt duża ilość gier. Spróbuj ponownie.')
-                elif player_1 is None or player_2 is None:
-                    write_color("red", f'Niepoprawny wynik! Wybierz graczy. Spróbuj ponownie.')
-                elif player_1 == player_2: 
-                    write_color("red", f'Niepoprawny wynik! Wybierz różnych graczy. Spróbuj ponownie.')
-                elif result_1 + result_2 == 0:
-                    write_color("yellow", f'Raczej nie ma sensu dodwać wyniku 0:0. Spróbuj ponownie.') 
-                else:
-                    write_color("green", f'Wynik dodany! Rezultat: {player_1} {result_1} : {result_2} {player_2}')
-                    save_result_to_db(player_1, result_1, result_2, player_2)
+                with col4:        
+                    player_2 = selectbox("Gracz 2", ["A", "B", "C"])
 
 
-        results_df = pd.read_csv('results.csv', header=0)
+                submitted = st.form_submit_button("Submit")
+                if submitted:
+                    if result_1 + result_2 > 5:
+                        st.write(f'Niepoprawny wynik! Zbyt duża ilość gier. Spróbuj ponownie.')
+                        write_color("red", f'Niepoprawny wynik! Zbyt duża ilość gier. Spróbuj ponownie.')
+                    elif player_1 is None or player_2 is None:
+                        write_color("red", f'Niepoprawny wynik! Wybierz graczy. Spróbuj ponownie.')
+                    elif player_1 == player_2: 
+                        write_color("red", f'Niepoprawny wynik! Wybierz różnych graczy. Spróbuj ponownie.')
+                    elif result_1 + result_2 == 0:
+                        write_color("yellow", f'Raczej nie ma sensu dodwać wyniku 0:0. Spróbuj ponownie.') 
+                    else:
+                        write_color("green", f'Wynik dodany! Rezultat: {player_1} {result_1} : {result_2} {player_2}')
+                        now = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+                        save_result_to_db(player_1, result_1, result_2, player_2, now)
 
-        st.dataframe(results_df)            
+            add_vertical_space(1)
+            st.markdown('Tabela rezultatów')
+            results_df = pd.read_csv('results.csv', header=0).sort_values(by=['Data'], ascending=False)
+
+            st.dataframe(results_df, use_container_width = True)            
 
 
 def create_dashboard():
